85a6d080afcd6dd9844b0c5ef788d5f409d3d1be
ALturky-pro <79476168+ALturky-pro@users.noreply.github.com>
2021-02-22 21:12:34 +0200

    Add files via upload

---
+++ ALturky pro.py
@@ -0,0 +1,72 @@
+import os,time
+os . system ("clear")
+import time,sys
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.01)
+		
+print ("\033[1;34mMohamed ALturky")
+jalan("Welcome to mt channel Mohamed ALturky WA linke whatsApp 01126796207")
+bb = """╔═══╗╔╗───╔════╗╔╗─╔╗╔═══╗╔╗╔═╗╔╗──╔╗╔═══╗╔═══╗╔═══╗
+║╔═╗║║║───║╔╗╔╗║║║─║║║╔═╗║║║║╔╝║╚╗╔╝║║╔═╗║║╔═╗║║╔═╗║
+║║─║║║║───╚╝║║╚╝║║─║║║╚═╝║║╚╝╝─╚╗╚╝╔╝║╚═╝║║╚═╝║║║─║║
+║╚═╝║║║─╔╗──║║──║║─║║║╔╗╔╝║╔╗║──╚╗╔╝─║╔══╝║╔╗╔╝║║─║║
+║╔═╗║║╚═╝║──║║──║╚═╝║║║║╚╗║║║╚╗──║║──║║───║║║╚╗║╚═╝║
+╚╝─╚╝╚═══╝──╚╝──╚═══╝╚╝╚═╝╚╝╚═╝──╚╝──╚╝───╚╝╚═╝╚═══╝"""
+print (bb)
+
+jalan("\033[1;35m#*"*22)
+jalan("\033[1;36m#*"*22)
+
+jalan("『01』>>>update termux") 
+
+jalan("『02』>>>thngis termux") 
+
+#jalan("『3』 meta") 
+
+#jalan("『4』 logo")
+
+#jalan("『5』 Iogo2")
+
+#jalan("『6』 exit")
+
+jalan("\033[1;36m#*"*22)
+jalan("\033[1;35m#*"*22)
+j =input("type Pleas:")
+if j == '1':
+    print("welcome")
+    import os
+    os . system("pkg install root-repo && pkg install && unstable-repo && pkg install x11-repo && apt update && termux-setup-storage -y && pkg install python -y && pkg install python2 -y && pkg install git -y && pkg install ruby -y && pkg install nmap -y && pkg install unzip -y && pkg install nano -y && apt install php -y && apt install golang -y && apt install nano -y && apt install hydra -y && apt install cmatrix -y && pkg install figlet -y && pkg install wget -y && pkg install wget -y && pkg install python2 -y && pkg install python2 -y && pkg install cowsay -y && pkg install toilet -y && pkg install ruby -y && pkg install unzip && pkg install curl -y && pkg install unzip -y && pkg install openssh -y && pkg install tor -y && pkg install unrar -y && pkg install clang -y && pkg install w3m -y && pkg install proot -y && pip2 install wget && pip2 install requests && pip2 install wget && pip2 install requests && clear && apt update && apt upgrade -y && clear && apt update && apt upgrade")
+
+if j == '2':
+    print("weat")
+    import os
+    os . system("clear")
+    print("Welcome to mt channel Mohamed ALturky WA linke whatsApp 01126796207")
+    jalan("『01』 meta") 
+    jalan("『02』 logo")
+    jalan("『03』 ngrok")
+    jalan("『04』 exit")
+    jalan("『05』ip wepsite")
+    l =input("type Pleas:")
+    if l == '01':
+        print("wate")
+        import os
+        os . system("pkg update && pkg upgrade && pkg install git curl wget nmap -y && curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod 777 metasploit.sh && ./metasploit.sh && bash metasploit.sh && clear")
+        k =input("Type msfconsole pleas:>>>≥")
+if l == '02':
+        print("wate")
+        import os
+        os . system(" pkg update && pkg upgrade ; git clone https://github.com/Amerlaceset/Virus4 && cd Virus4 && python2 Virus4.py")
+if l == '03':
+        print("wate")
+        import os
+        os . system("pkg update && pkg upgrade  && cd /sdcard && ls && mv ngrok-stable-linux-arm.zip $HOME && cd && ls && unzip ngrok-stable-linux-arm.zip && rm -rif ngrok-stable-linux-arm.zip && ls")
+
+if l == '05':
+        print("wate")
+        import os
+        os . system("git clone https://github.com/MOHAMED-alturky-wa/Mohamed-ALturky-ip.git ; cd Mohamed-ALturky-ip ; 06thon3 Mohamed-ALturky-ip.py")
